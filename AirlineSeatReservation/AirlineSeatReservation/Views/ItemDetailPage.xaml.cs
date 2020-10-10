a3867ffbd9bfeb9e7da2067cbfb03d152c1b3e0a
Felipe S. Fernandes <feleast.49@gmail.com>
2020-10-10 18:26:28 -0300

    Add project files.

---
+++ AirlineSeatReservation/AirlineSeatReservation/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using AirlineSeatReservation.ViewModels;
+
+namespace AirlineSeatReservation.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
